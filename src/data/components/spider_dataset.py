dc3b3ed824137a4d5e79d3264030fb4d622bb301
HGX Server <21020205@vnu.edu.vn>
2024-06-13 11:42:39 +0900

    from project init to 0.87 mAP

--- src/data/components/spider_dataset.py
+++ src/data/components/spider_dataset.py
@@ -1,24 +1,39 @@
+from typing import Optional, List
 import monai
+import monai.transforms
 import SimpleITK as sitk
+import monai.transforms.utility
+import monai.transforms.utility.dictionary
 import numpy as np 
 import torch
 from lightning import LightningDataModule
 from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
-from torchvision.datasets import MNIST
 from torchvision.transforms import transforms
 import json
 import os
+import rootutils
+
+rootutils.setup_root(search_from=__file__, indicator="setup.py", pythonpath=True)
+from src.data.transforms import array
+from src.data.transforms import utils
+
 # always starting with vanilla dataset, like its a norm to me now
 class SpiderDataset(Dataset):
     num_class = 15
     def __init__(self, 
-                 data_dir: str, 
-                 json_path: str,
+                 data = None,
+                 data_dir: str = "", 
+                 json_path: str = "",
                  ):
         super().__init__()
         self.data = list()
         self.data_dir = data_dir
-        self.setup(json_path)
+        if data is not None:
+            self.data = data
+        else:
+            if data_dir == "" or json_path == "":
+                raise AssertionError("No dataset ?")
+            self.setup(json_path)
     
     def setup(self, json_path):
         json_object = json.load(open(json_path, "r"))
@@ -32,46 +47,104 @@ class SpiderDataset(Dataset):
             except:
                 raise InsertionError("Something wrong with json file, cannot load or do anything, at all")
     
-    def __getitem__(self, index):
+    def get_item(self, index: int):
         output = dict()
-        output["input"] = ""
-        output["output"] = ""
+        output["image"] = ""
+        output["label"] = ""
         if isinstance(self.data[index]["image"], list):
-            output["input"] = [os.path.join(self.data_dir, image) for image in self.data[index]["image"]]
+            output["image"] = [os.path.join(self.data_dir, image) for image in self.data[index]["image"]]
         else:
-            output["input"] = os.path.join(self.data_dir, self.data[index]["image"])
-        output["output"] = os.path.join(self.data_dir, self.data[index]["label"])
+            # Add for debugging
+            path = os.path.join(self.data_dir, self.data[index]["image"])
+            output["image"] = [path] ##[path, path] ##[path, path, path, path] ##Set to 4 because the model_inferr in validation_step
+        output["label"] = os.path.join(self.data_dir, self.data[index]["label"])
+        return output
+    
+    # In case they query in list of index
+    def __getitem__(self, index):
+        output = None
+        if not isinstance(index, int):
+            output = []
+            for id in index:
+                output.append(self.get_item(id))
+        else:
+            output = self.get_item(int(index))
+        
         return output
     
     def __len__(self) -> int:
         return len(self.data)
     
     
-
 class SpiderTransformedDataset(Dataset):
     def __init__(self, 
                  dataset: SpiderDataset,
                  transform: monai.transforms.Compose):
+        super().__init__()
         self.dataset = dataset
         self.transform = transform
     
     def __getitem__(self, index):
-        return self.transform(self.dataset[index])
+        transformed = self.transform(self.dataset[index])
+        # if transformed["image"].ndim == 3:
+        #     transformed["image"] = transformed["image"].unsqueeze(0)
+
+        return transformed
     
     def __len__(self) -> int:
         return len(self.dataset)
 
 if __name__=="__main__":
-    dataset = SpiderDataset(data_dir = "./data/dataset/spine_nii", json_path="./data/jsons/spine_v2.json")
-    transform = monai.transforms.Compose([monai.transforms.LoadImaged(keys=["image", "label"]),
-                                          monai.transforms.ConvertToMultiChannelBasedOnSpiderClassesd(keys=["label"]),
+    dataset = SpiderDataset(data_dir = "./data/dataset/spine_nii", json_path="./data/jsons/spine_v3.json")
+    # dataset = SpiderDataset(data_dir = "./data/dataset", json_path="/data/hpc/spine/jsons/brats21_folds_one.json")
+    
+    transform = monai.transforms.Compose([monai.transforms.LoadImaged(keys=["image", "label"], image_only = False),
+                                          array.ConvertToMultiChannelBasedOnSpiderClassesdSemantic(keys=["label"]),
+                                        #   monai.transforms.ConvertToMultiChannelBasedOnBratsClassesd(keys=["label"]),
+                                        #   monai.transforms.Resized(keys=["image", "label"], spatial_size=(250, 250, 155)),
                                           monai.transforms.ToTensord(keys=["image", "label"]),])
     
+    # transform = transforms.Compose(
+    #     [
+    #         monai.transforms.LoadImaged(keys=["image", "label"], image_only=False),
+    #         monai.transforms.ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
+    #         # utils.AddChanneld(keys=["image"]),
+    #         monai.transforms.EnsureChannelFirstd(keys="image", channel_dim='no_channel'),
+    #         monai.transforms.CropForegroundd(
+    #             keys=["image", "label"], source_key="image", k_divisible=[96, 96, 96]
+    #         ),
+    #         monai.transforms.RandSpatialCropd(
+    #             keys=["image", "label"], roi_size=[96, 96, 96], random_size=False
+    #         ),
+    #         monai.transforms.RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=0),
+    #         monai.transforms.RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=1),
+    #         monai.transforms.RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=2),
+    #         monai.transforms.NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
+    #         monai.transforms.RandScaleIntensityd(keys="image", factors=0.1, prob=1.0),
+    #         monai.transforms.RandShiftIntensityd(keys="image", offsets=0.1, prob=1.0),
+    #         monai.transforms.ToTensord(keys=["image", "label"]),
+    #     ]
+    # )
+
     transformed = SpiderTransformedDataset(dataset, transform)
     data = dataset[0]
-    images = transformed[0]
+    images = transformed[12]
     print(data)
-    print(images["input"].size(), images["input"].dtype)
-    print(images["output"].size(), images["output"].dtype)
-        
-        
+    print(images["image"].size(), images["image"].dtype)
+    print(images["label"].size(), images["label"].dtype)
+    print(images.keys())
+    print("------------------")
+    print(images['image_meta_dict'])
+    print("------------------")
+    print(images['label_meta_dict'])
+    config = images['label_meta_dict']
+    with open("/work/hpc/spine-segmentation/data/config.json", "w") as f:
+        f.write(str(config))
+
+    print(images["label"].size())
+    
+    # res = 0
+    # for image in transformed:
+    #     res = max(res, image["label"].size(0))
+    
+    # print(res)
--- src/models/brats21_module.py
+++ src/models/brats21_module.py
@@ -20,6 +20,7 @@ import os
 import shutil
 
 from functools import partial
+import wandb
     
 def dice(x, y):
     intersect = np.sum(np.sum(np.sum(x * y)))
@@ -118,24 +119,21 @@ class Brats21LitModule(LightningModule):
         
         self.dice_acc = DiceMetric(include_background=True, reduction=MetricReduction.MEAN_BATCH, get_not_nans=True)
         self.post_sigmoid = Activations(sigmoid=True)
-        self.post_pred = AsDiscrete(argmax=False, logit_thresh=0.5)
-
-        # self.amp = amp
-        # self.scaler = None
-        # if self.amp:
-        #     self.scaler = GradScaler()
+        self.post_pred = AsDiscrete(argmax=False, threshold=0.5)
+        
+        self.val_acc_max = 0
 
         # loss function
         self.criterion = DiceLoss(to_onehot_y=False, sigmoid=True)
 
         # metric objects for calculating and averaging accuracy across batches
-        # self.train_acc = Accuracy(task="multiclass", num_classes=10)
+        self.train_acc = AverageMeter()
         self.val_acc = AverageMeter()
         self.test_acc = AverageMeter()
 
         # for averaging loss across batches
-        self.train_loss = AverageMeter()
-        self.val_loss = AverageMeter()
+        self.train_loss = MeanMetric()
+        self.val_loss = MeanMetric()
         # self.test_loss = AverageMeter()
 
         # for tracking best so far validation accuracy
@@ -160,12 +158,14 @@ class Brats21LitModule(LightningModule):
 
     def on_train_epoch_start(self) -> None:
         self.net.train()
+        self.train_acc.reset()
     
     def on_train_epoch_end(self) -> None:
         for param in self.net.parameters():
             param.grad = None
         
-        self.train_loss.reset()
+        # self.train_loss.reset()
+        
     
     def model_step(
         self, batch: Tuple[torch.Tensor, torch.Tensor]
@@ -192,8 +192,8 @@ class Brats21LitModule(LightningModule):
             data, target = batch
         else:
             data, target = batch["image"], batch["label"]
-        
-        data, target = data.cuda(0), target.cuda(0)
+        # print(data.size())
+        data, target = data.cuda(0), target.cuda(0) #??? vl fix cứng 
             
         for param in self.net.parameters():
             param.grad = None
@@ -218,6 +218,37 @@ class Brats21LitModule(LightningModule):
         #     param.grad = None
         # return self.train_loss.avg
         
+        # with torch.no_grad(): # ????
+        #     # for idx, batch in enumerate(loader):
+        #     data, target = batch["image"], batch["label"]
+        #     data, target = data.cuda(0), target.cuda(0)
+        #     with autocast(enabled=False):
+        #         logits = self.model_inferer(data)
+        #     train_labels_list = decollate_batch(target) ## Optimal to use decollate_batch
+        #     train_outputs_list = decollate_batch(logits) ## Optimal to use decollate_batch
+        #     train_output_convert = [self.post_pred(self.post_sigmoid(val_pred_tensor)) for val_pred_tensor in train_outputs_list]
+        #     self.dice_acc.reset()
+        #     self.dice_acc(y_pred=train_output_convert, y=train_labels_list)
+        #     acc, not_nans = self.dice_acc.aggregate()
+        #     acc = acc.cuda(0)
+
+        #     self.train_acc.update(acc.cpu().numpy(), n=not_nans.cpu().numpy())
+            
+        #     Dice_TC = self.train_acc.avg[0]
+        #     Dice_WT = self.train_acc.avg[1]
+        #     Dice_ET = self.train_acc.avg[2]
+
+        #     # loss = self.criterion(logits, target)
+            
+        #     # self.val_loss.update(loss, data.size(0))
+        #     # self.log("train/loss", loss, on_step=True, on_epoch=True, prog_bar=True)
+        #     # self.log("val/acc", np.mean(self.val_acc.avg) , on_step=True, on_epoch=True, prog_bar=False, logger=False) ##Mean Val Dice
+        #     # self.log("val/acc_best", np.mean(self.val_acc.avg), sync_dist=True, prog_bar=True)
+        #     self.log("train/Dice_TC", Dice_TC, on_step=True, on_epoch=True, prog_bar=True)
+        #     self.log("train/Dice_WT", Dice_WT, on_step=True, on_epoch=True, prog_bar=True)
+        #     self.log("train/Dice_ET", Dice_ET, on_step=True, on_epoch=True, prog_bar=True)
+        #     print("Dice_Train_Mean: {:.6f}".format(np.mean(self.train_acc.avg)))
+        
         return loss, logits, target
 
     def training_step(
@@ -233,14 +264,15 @@ class Brats21LitModule(LightningModule):
         loss, logits, targets = self.model_step(batch)
         
         # update and log metrics
-        self.train_loss.update(loss, batch["image"].shape(0))
+        self.train_loss(loss)
         # self.train_acc(preds, targets)
-        self.log("train/loss", self.train_loss.avg, on_step=True, on_epoch=True, prog_bar=True)
+        self.log("train/loss", self.train_loss, on_step=True, on_epoch=True, prog_bar=True)
         # self.log("train/acc", self.train_acc, on_step=False, on_epoch=True, prog_bar=True)
 
         # return loss or backpropagation will fail
         return loss
 
+    @torch.no_grad()
     def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> None:
         """Perform a single validation step on a batch of data from the validation set.
 
@@ -266,14 +298,27 @@ class Brats21LitModule(LightningModule):
             data, target = data.cuda(0), target.cuda(0)
             with autocast(enabled=False):
                 logits = self.model_inferer(data)
-            val_labels_list = decollate_batch(target)
-            val_outputs_list = decollate_batch(logits)
+            val_labels_list = decollate_batch(target) ## Optimal to use decollate_batch, we can choose to use it or not
+            val_outputs_list = decollate_batch(logits) ## Optimal to use decollate_batch, we can choose to use it or not
+            
+            # print(logits.shape)
+            # print(logits)
+    
+            # print(type(val_outputs_list))
+            # print(len(val_outputs_list))
             val_output_convert = [self.post_pred(self.post_sigmoid(val_pred_tensor)) for val_pred_tensor in val_outputs_list]
+            # print("++++++++++++++++++++++++++")
+            print(val_output_convert)
             self.dice_acc.reset()
             self.dice_acc(y_pred=val_output_convert, y=val_labels_list)
+            # print(self.dice_acc(y_pred=val_output_convert, y=val_labels_list))
             acc, not_nans = self.dice_acc.aggregate()
             acc = acc.cuda(0)
+            # print("+++++++++")
+            # print(acc)
 
+            self.val_acc.update(acc.cpu().numpy(), n=not_nans.cpu().numpy())
+            
             Dice_TC = self.val_acc.avg[0]
             Dice_WT = self.val_acc.avg[1]
             Dice_ET = self.val_acc.avg[2]
@@ -281,26 +326,23 @@ class Brats21LitModule(LightningModule):
             loss = self.criterion(logits, target)
             
             self.val_loss.update(loss, data.size(0))
-            self.log_dict({"Dice_TC": Dice_TC, "Dice_WT": Dice_WT, "Dice_ET": Dice_ET, "batch_id": batch_idx})
-            self.log("val/loss", self.val_loss.avg, on_step=True, on_epoch=True, prog_bar=True)
-            # print(
-            #     ", Dice_TC:",
-            #     Dice_TC,
-            #     ", Dice_WT:",
-            #     Dice_WT,
-            #     ", Dice_ET:",
-            #     Dice_ET,
-            #     ", time {:.2f}s".format(time.time() - start_time),
-            # )
-            # start_time = time.time()
+            self.log("val/loss", loss, on_step=True, on_epoch=True, prog_bar=True)
+            # self.log("val/acc", np.mean(self.val_acc.avg) , on_step=True, on_epoch=True, prog_bar=False, logger=False) ##Mean Val Dice
+            # self.log("val/acc_best", np.mean(self.val_acc.avg), sync_dist=True, prog_bar=True)
+            self.log("val/Dice_TC", Dice_TC, on_step=True, on_epoch=True, prog_bar=True)
+            self.log("val/Dice_WT", Dice_WT, on_step=True, on_epoch=True, prog_bar=True)
+            self.log("val/Dice_ET", Dice_ET, on_step=True, on_epoch=True, prog_bar=True)
 
+            # print("Val Dice: Mean: {:.6g}, TC: {:.6f}, WT: {:.6f}, ET: {:.6f}".format(np.mean(self.val_acc.avg), Dice_TC, Dice_WT, Dice_ET))
         # return run_acc.avg
+        return {'loss': loss, 'Dice_TC': Dice_TC, 'Dice_WT': Dice_WT, 'Dice_ET': Dice_ET}
 
     def on_validation_epoch_start(self) -> None:
         self.net.eval()
-        self.val_loss.reset()
+        # self.val_loss.reset()
         self.val_acc.reset()
     
+    @torch.no_grad()
     def on_validation_epoch_end(self) -> None:
         "Lightning hook that is called when a validation epoch ends."
         # acc = self.val_acc.compute()  # get current val acc
@@ -314,38 +356,15 @@ class Brats21LitModule(LightningModule):
         Dice_TC = val_acc[0]
         Dice_WT = val_acc[1]
         Dice_ET = val_acc[2]
-        print(
-            "Final validation stats {}/{}".format(self.current_epoch, self.trainer.max_epochs - 1),
-            ", Dice_TC:",
-            Dice_TC,
-            ", Dice_WT:",
-            Dice_WT,
-            ", Dice_ET:",
-            Dice_ET,
-        )
 
-        # if writer is not None:
-        #     writer.add_scalar("Mean_Val_Dice", np.mean(val_acc), epoch)
-        #     if semantic_classes is not None:
-        #         for val_channel_ind in range(len(semantic_classes)):
-        #             if val_channel_ind < val_acc.size:
-        #                 writer.add_scalar(semantic_classes[val_channel_ind], val_acc[val_channel_ind], epoch)
-        
         val_avg_acc = np.mean(val_acc)
+        self.log("val/acc", val_avg_acc, sync_dist=True, prog_bar=True, logger=False) ##Mean Val Dice
         
-        if val_avg_acc > val_acc_max:
-            print("new best ({:.6f} --> {:.6f}). ".format(val_acc_max, val_avg_acc))
-            val_acc_max = val_avg_acc
-            b_new_best = True
-            if self.trainer.log_dir is not None:
-                self.save_checkpoint(
-                    self.net, self.current_epoch, best_acc=val_acc_max, optimizer=self.hparams.optimizer, scheduler=self.hparams.scheduler
-                )
-                
-            self.save_checkpoint(self.net, self.current_epoch, best_acc=val_acc_max, filename="model_final.pt")
-            if b_new_best:
-                print("Copying to model.pt new best model!!!!")
-                shutil.copyfile(os.path.join(self.trainer.log_dir, "model_final.pt"), os.path.join(self.trainer.log_dir, "model.pt"))
+        if val_avg_acc > self.val_acc_max:
+            print("New best ({:.6f} --> {:.6f}). ".format(self.val_acc_max, val_avg_acc))
+            self.val_acc_max = val_avg_acc
+            
+        # print(val_avg_acc)
         
 
     def test_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> None:
@@ -385,7 +404,9 @@ class Brats21LitModule(LightningModule):
 
         :return: A dict containing the configured optimizers and learning-rate schedulers to be used for training.
         """
+        
         optimizer = self.hparams.optimizer(params=self.trainer.model.parameters())
+        
         if self.hparams.scheduler is not None:
             scheduler = self.hparams.scheduler(optimizer=optimizer)
             return {
@@ -403,6 +424,9 @@ class Brats21LitModule(LightningModule):
     def save_checkpoint(self, model, epoch, filename="model.pt", best_acc=0, optimizer=None, scheduler=None):
         state_dict = model.state_dict()
         save_dict = {"epoch": epoch, "best_acc": best_acc, "state_dict": state_dict}
+        optimizer = self.hparams.optimizer(params=self.trainer.model.parameters())
+        scheduler = self.hparams.scheduler(optimizer=optimizer)
+        
         if optimizer is not None:
             save_dict["optimizer"] = optimizer.state_dict()
         if scheduler is not None:
