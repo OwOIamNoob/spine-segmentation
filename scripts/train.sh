05aa686e6a0e11194bd36876a505de8a3d8be70e
OwOIamNoob <21020205@vnu.edu.vn>
2024-08-13 20:23:23 +0900

    Configuration for Adaptation

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,4 +2,4 @@ export WANDB_API_KEY=08610216d8143c15ddb5d2f16b6d432fefa2c827
 export CUDA_VISIBLE_DEVICES=0
 python '/work/hpc/spine-segmentation/src/train.py' logger=wandb trainer.max_epochs=300 \
                                                     trainer.check_val_every_n_epoch=2 \
-                                                    logger.wandb.name=Refine_callback
+                                                    logger.wandb.name=Mean_CELoss
